9d36ed08331dce34a2351c0352abcb330f2373bd
donFalconJefe <sevileelvis@gmail.com>
2021-11-27 19:36:22 +0300

    Initial

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:bmi/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BMI Calc',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+        visualDensity: VisualDensity.adaptivePlatformDensity
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
